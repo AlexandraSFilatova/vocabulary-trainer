fd64491990c62f91629c3dff3c3ccd712a9ab903
AlexandraKovalenok <alexandrakovalenok@gmail.com>
2022-09-09 16:50:45 +0400

    the ability to repeat previously trained words was added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # vocabulary-trainer
+The project is under active development.
+This code works with .csv files FROM A LOCAL DIRECTORY(thus, this code will not work in such environments as Colab without any modifications)
+Files with words from the folder "Content" contain a following structure: Date add, Word, Translation, Date of the last training, Training status, Number of training.
+
+
+Date add - date when a word was added
+
+Word - word in English
+
+Translation - word in native language (the current set is Russian)
+
+Date of the last training - Date of the last training
+
+Training status - L means "learned", T means "need to train"
+
+Number of training - Number of training. Default number MUST BE 0.
+
+To run code you must have Python 3 on your Windows PC (MacOS,Linux not tested yet). You can just run "start.cmd" or run code from "vocabulary_trainer" with a program like VS Code through the opening directory (a function os.getcwd() will get a correct folder path only in this opening way).
+All other files and folders also should be downloaded to your PC (requirements.txt, Content etc.). You can add a new file with words, which has appropriate structure as described above, in the folder "Content".
--- vocabulary_trainer.py
+++ vocabulary_trainer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """
+Project is in progress.
 This code works with .csv files FROM LOCAL DERICTORIES.
 Files contain following structure: Date add, Word, Translation, Date of the last training, Training status, Number of training.
 
@@ -14,9 +15,10 @@ Date of the last training - Date of the last training
 
 Training status - L means "learned", T means "need to train"
 
-Number of training - Number of training
+Number of training - Number of training. Default number MUST BE 0
 """
 
+from asyncio.windows_events import NULL
 import io
 import os
 from IPython.display import Audio
@@ -138,26 +140,97 @@ def training():
         ex_4(word)
         print("\n")
 
+
+
+
+def Step6():#Step 6. File rewrite. Rewrite changed status and last training date
+    vocabulary.to_csv(os.path.join(folder_path, t_file), 
+                    index=False, 
+                    encoding="utf-8-sig")
+
+
+
+
+
+
+
 #Step 1. File selection for training.
-print("Please enter the folder path, where files with words were placed.")
+
 folder_path = os.path.join(os.path.abspath(os.getcwd()), "Content")
 files = [f for f in listdir(folder_path) 
-             if isfile(join(folder_path, f))]
+            if isfile(join(folder_path, f))] ##list files in folder
 print("Choose the file's number, which you want to train:")
 for file in files:
     print(str((files.index(file)+1)) + " - " + file)
 
 file_number = int(input("Enter the number: "))
 t_file = files[file_number -1]                                               #file under study
-vocabulary = pd.read_csv(os.path.join(folder_path, t_file))
+vocabulary = pd.read_csv(os.path.join(folder_path, t_file)) #read file and create DF
+
+
+#Step 1.1. Checking if any words need to be trained. These words are all words with a status "T". 
+# The first 3 trainings will be every day, all next ones will be every 2 days until words` status will change to "L". This part of the code will check words 
+# and ask about a repetition of words, if no, then it goes to the main training, if yes, it provides repetition, and then ask about the main training.
+try:
+    df_sample = vocabulary[(vocabulary["Training status"]=="T")&(vocabulary["Date of the last training"].notnull())].filter(items = ["Word", "Translation","Date of the last training","Number of training"]) 
+    df_sample["Date_diff"] = pd.to_numeric((date.today() - pd.to_datetime(df_sample["Date of the last training"]).dt.date).dt.days, downcast='integer')
+    df_sample = df_sample[(((df_sample["Number of training"] < 3)&(df_sample["Date_diff"] >= 1) )| ((df_sample["Number of training"] >= 3)&(df_sample["Date_diff"] >= 2)))].sample(n=5)
+    #random 5 unlearned words from file under study
+except:
+    df_sample = vocabulary[(vocabulary["Training status"]=="T")&(vocabulary["Date of the last training"].notnull())].filter(items = ["Word", "Translation","Date of the last training","Number of training"])
+    df_sample["Date_diff"] = pd.to_numeric((date.today() - pd.to_datetime(df_sample["Date of the last training"]).dt.date).dt.days, downcast='integer') 
+    df_sample = df_sample[(((df_sample["Number of training"] < 3)&(df_sample["Date_diff"] >= 1) )| ((df_sample["Number of training"] >= 3)&(df_sample["Date_diff"] >= 2)))]
+    #all unlearned words from file under study, when word`s amount is less than 5
+t_words = df_sample["Word"].to_list()                                          #sample to train 
+t_translate = df_sample["Translation"].to_list()                                      #translation of sample to train
+    
+dic_wt = dict(zip(t_words, t_translate)) #dic word:translation
+dic_tw = dict(zip(t_translate, t_words)) #dic translation:word
+
+if not t_words:
+    print("There are no words to repeat. Go to the main training.")
+else:
+    print("There are words to repeat. Do you want to repeat them? yes/no")
+    user_choice = str(input())
+    if user_choice.lower() == "yes":
+                    #Step 1.3. Training.
+            user_continue = "yes"
+            while (user_continue.lower() == "yes"):
+                training()
+                #Step 1.4-1.5. Ask to repeat training and to change training status.
+                print("Do you want to repeat training with these words? yes/no")
+                user_continue = str(input())
+                print("\n")
+                
+            print("Have you learned any of these words? yes/no")
+                ##chosen words won`t be participate in futher trainings
+            print("\n")
+            for word in t_words:
+                vocabulary.loc[vocabulary[vocabulary["Word"] == word].index, "Date of the last training"] = date.today()
+                vocabulary.loc[vocabulary[vocabulary["Word"] == word].index, "Number of training"] += 1
+                print(word)
+                print("\n")
+                user_choice = str(input())
+                print("\n")
+                if user_choice.lower() == "yes":
+                    vocabulary.loc[vocabulary[vocabulary["Word"] == word].index, "Training status"] = "L"
+                elif user_choice == "no":
+                    print("The word will be trained in further training")
+                else:
+                    print("Incorrect choice. Please write yes or no")
+            print("Repeating is over. Do you want to Lear new words?yes/no")
+            user_choice = str(input())
+            if user_choice.lower() == "no":
+                Step6() #rewrite changes in file 
+                quit()
 
 
 #Step 2. Sample formation.
 try:
-    df_sample = vocabulary[vocabulary["Training status"]=="T"].filter(items = ["Word", "Translation"]).sample(n=5) 
+    df_sample = vocabulary[(vocabulary["Training status"]=="T")&(vocabulary["Date of the last training"].isnull())].filter(items = ["Word", "Translation"]).sample(n=5) 
     #random 5 unlearned words from file under study
 except:
-    df_sample = vocabulary[vocabulary["Training status"]=="T"].filter(items = ["Word", "Translation"]) 
+    df_sample = vocabulary[(vocabulary["Training status"]=="T")&(vocabulary["Date of the last training"].isnull())].filter(items = ["Word", "Translation"]) 
     #all unlearned words from file under study, when word`s amount is less than 5
 t_words = df_sample["Word"].to_list()                                          #sample to train 
 t_translate = df_sample["Translation"].to_list()                                      #translation of sample to train
@@ -167,7 +240,7 @@ dic_tw = dict(zip(t_translate, t_words)) #dic translation:word
 
 #Step 3. Training.
 user_continue = "yes"
-while (user_continue == "yes"):
+while (user_continue.lower() == "yes"):
     training()
     #Step 4-5. Ask to repeat training and to change training status.
     print("Do you want to repeat training with these words? yes/no")
@@ -179,21 +252,21 @@ print("Have you learned any of these words? yes/no")
 print("\n")
 for word in t_words:
     vocabulary.loc[vocabulary[vocabulary["Word"] == word].index, "Date of the last training"] = date.today()
+    vocabulary.loc[vocabulary[vocabulary["Word"] == word].index, "Number of training"] += 1
     print(word)
     print("\n")
     user_choice = str(input())
     print("\n")
-    if user_choice == "yes":
+    while user_choice.lower() !=  "yes" and user_choice.lower() != "no":
+        print("Incorrect choice. Please write yes or no.")
+        user_choice = str(input())
+    if user_choice.lower() == "yes":
         vocabulary.loc[vocabulary[vocabulary["Word"] == word].index, "Training status"] = "L"
     elif user_choice == "no":
         print("The word will be trained in further training")
-    else:
-        print("Incorrect choice. Please write yes or no")
+        
 print("Training is over.")
 
 
-#Step 6. File rewrite.
-vocabulary.to_csv(os.path.join(folder_path, t_file), 
-                  index=False, 
-                  encoding="utf-8-sig")
+Step6()
 
